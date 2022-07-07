b70c230a377b2a1508adeeecf905c943799534da
Josh Fung <joshuagyfung@gmail.com>
2022-07-06 21:44:31 -0700

    Fixed settings.py, created migration for Product model

--- display_site/settings.py
+++ display_site/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scraping.config.ScrapingConfig',
+    'scraping.apps.ScrapingConfig',
 ]
 
 MIDDLEWARE = [
