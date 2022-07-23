223d9b7a4d2d7cbeb0a211655d7ebfbf02a81ec9
Josh Fung <joshuagyfung@gmail.com>
2022-07-22 20:04:57 -0700

    Updated models.py

--- scraping/models.py
+++ scraping/models.py
@@ -5,13 +5,10 @@ class Product(models.Model):
     store = models.CharField(max_length=25)
     name = models.CharField(max_length=350)
     brand = models.CharField(max_length=50)
-    link = models.CharField(max_length=2083, default="", unique=True)
     normal_price = models.DecimalField(max_digits=7, decimal_places=2)
     sale_price = models.DecimalField(max_digits=7, decimal_places=2)
     rating = models.DecimalField(max_digits=2, decimal_places=1)
     num_of_ratings = models.IntegerField()
-    shipping = models.DecimalField(max_digits=7, decimal_places=2)
+    shipping = models.CharField(max_length=25)
     promotion = models.CharField(max_length=150)
     out_of_stock = models.BooleanField()
-
-
