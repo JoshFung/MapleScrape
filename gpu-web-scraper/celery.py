6de60843cec6be41114d0687d42cd2fa85463efb
Josh Fung <joshuagyfung@gmail.com>
2022-07-22 20:04:21 -0700

    Added beat schedule

--- gpu-web-scraper/celery.py
+++ gpu-web-scraper/celery.py
@@ -2,10 +2,9 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'display_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gpu-web-scraper.settings')
 
 app = Celery('gpu-web-scraper')
-app.conf.timezone('Canada/Pacific')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -15,3 +14,11 @@ app.config_from_object('django.conf:settings', namespace='celery')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    # executes every 10 minute
+    'scraping-task-ten-min': {
+        'task': 'scraping.tasks.scrape',
+        'schedule': crontab(minute='*/10')
+    }
+}
