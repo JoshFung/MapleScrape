cac1a17161967cba566cfefd5e795ac7b2bd72c6
Josh Fung <joshuagyfung@gmail.com>
2022-07-22 20:03:55 -0700

    Moved files

--- gpu-web-scraper/urls.py
+++ gpu-web-scraper/urls.py
@@ -1,4 +1,4 @@
-"""display_site URL Configuration
+"""gpu-web-scraper URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# from .views import HomePageView
-from . import views
+from .views import HomePageView
 
 urlpatterns = [
     # TODO: understand better
-    path('', views.index, name='home'),
+    path('', HomePageView.as_view(), name='home'),
     path('admin/', admin.site.urls),
 ]
