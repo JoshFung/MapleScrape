2ae9d3a26ed3fd78cdbe68f4c00bad35531af0bf
Josh Fung <joshuagyfung@gmail.com>
2022-07-22 20:38:35 -0700

    Make all fields nullable

--- scraping/models.py
+++ scraping/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Product(models.Model):
-    store = models.CharField(max_length=25)
-    name = models.CharField(max_length=350)
-    brand = models.CharField(max_length=50)
+    store = models.CharField(max_length=25, null=True)
+    name = models.CharField(max_length=350, null=True)
+    brand = models.CharField(max_length=50, null=True)
     normal_price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
     sale_price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
     rating = models.DecimalField(max_digits=2, decimal_places=1, null=True)
