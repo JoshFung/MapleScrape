6f5d86ff004459f57bd47b76a57b3d9cd0181992
Josh Fung <joshuagyfung@gmail.com>
2022-08-07 22:51:36 -0700

    Removed comments

--- gpu-web-scraper/urls.py
+++ gpu-web-scraper/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from .views import HomePageView
 
 urlpatterns = [
-    # TODO: understand better
     path('', HomePageView.as_view(), name='home'),
     path('admin/', admin.site.urls),
 ]
